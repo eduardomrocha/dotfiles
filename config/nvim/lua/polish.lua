e129a7245d5af50b37a6e70b13e8a0b4e96c8b4c
Eduardo Menezes Rocha <eduardomenezesr@gmail.com>
2025-07-18 12:43:37 -0300

    feat: new buffers will be inserted in order in the tabline

--- config/nvim/lua/polish.lua
+++ config/nvim/lua/polish.lua
@@ -1,25 +1,18 @@
--- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  polish = function()
+    vim.opt.clipboard = "unnamedplus"
 
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
+    if vim.fn.has "wsl" == 1 then
+      vim.api.nvim_create_autocmd("TextYankPost", {
+        group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+        callback = function() vim.fn.system("clip.exe", vim.fn.getreg '"') end,
+      })
+    end
 
--- Set up custom filetypes
--- vim.filetype.add {
---   extension = {
---     foo = "fooscript",
---   },
---   filename = {
---     ["Foofile"] = "fooscript",
---   },
---   pattern = {
---     ["~/%.config/foo/.*"] = "fooscript",
---   },
--- }
-vim.opt.clipboard = "unnamedplus"
-if vim.fn.has "wsl" == 1 then
-  vim.api.nvim_create_autocmd("TextYankPost", {
-    group = vim.api.nvim_create_augroup("Yank", { clear = true }),
-    callback = function() vim.fn.system("clip.exe", vim.fn.getreg '"') end,
-  })
-end
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "AstroBufsUpdated",
+      group = vim.api.nvim_create_augroup("auto_sort_tabline", { clear = true }),
+      callback = function() require("astronvim.utils.buffer").sort("bufnr", true) end,
+    })
+  end,
+}
